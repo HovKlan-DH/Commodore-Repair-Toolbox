cf9bdde29d2747a5c11c5fcaca59cfb469718deb
Dennis <74433222+HovKlan-DH@users.noreply.github.com>
2023-09-10 01:00:54 +0200

    Update

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabMain = new System.Windows.Forms.TabPage();
             this.tabTrivia = new System.Windows.Forms.TabPage();
@@ -35,7 +36,23 @@
             this.tabLinks = new System.Windows.Forms.TabPage();
             this.tabSettings = new System.Windows.Forms.TabPage();
             this.tabAbout = new System.Windows.Forms.TabPage();
+            this.groupBoxList = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panelImageList = new System.Windows.Forms.Panel();
             this.tabControl1.SuspendLayout();
+            this.tabMain.SuspendLayout();
+            this.groupBoxList.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -49,55 +66,56 @@
             this.tabControl1.Controls.Add(this.tabLinks);
             this.tabControl1.Controls.Add(this.tabSettings);
             this.tabControl1.Controls.Add(this.tabAbout);
-            this.tabControl1.Location = new System.Drawing.Point(187, 12);
+            this.tabControl1.Location = new System.Drawing.Point(235, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(564, 529);
+            this.tabControl1.Size = new System.Drawing.Size(935, 679);
             this.tabControl1.TabIndex = 0;
             // 
             // tabMain
             // 
+            this.tabMain.Controls.Add(this.panel1);
+            this.tabMain.Controls.Add(this.groupBoxList);
             this.tabMain.Location = new System.Drawing.Point(4, 25);
             this.tabMain.Name = "tabMain";
             this.tabMain.Padding = new System.Windows.Forms.Padding(3);
-            this.tabMain.Size = new System.Drawing.Size(556, 500);
+            this.tabMain.Size = new System.Drawing.Size(927, 650);
             this.tabMain.TabIndex = 0;
             this.tabMain.Text = "Main";
             this.tabMain.UseVisualStyleBackColor = true;
             // 
             // tabTrivia
             // 
+            this.tabTrivia.BackColor = System.Drawing.Color.Transparent;
             this.tabTrivia.Location = new System.Drawing.Point(4, 25);
             this.tabTrivia.Name = "tabTrivia";
             this.tabTrivia.Padding = new System.Windows.Forms.Padding(3);
-            this.tabTrivia.Size = new System.Drawing.Size(374, 397);
+            this.tabTrivia.Size = new System.Drawing.Size(927, 650);
             this.tabTrivia.TabIndex = 1;
             this.tabTrivia.Text = "Trivia";
-            this.tabTrivia.UseVisualStyleBackColor = true;
             // 
             // tabTroubleshooting
             // 
+            this.tabTroubleshooting.BackColor = System.Drawing.Color.WhiteSmoke;
             this.tabTroubleshooting.Location = new System.Drawing.Point(4, 25);
             this.tabTroubleshooting.Name = "tabTroubleshooting";
-            this.tabTroubleshooting.Size = new System.Drawing.Size(374, 397);
+            this.tabTroubleshooting.Size = new System.Drawing.Size(927, 650);
             this.tabTroubleshooting.TabIndex = 2;
             this.tabTroubleshooting.Text = "Troubleshooting";
-            this.tabTroubleshooting.UseVisualStyleBackColor = true;
             // 
             // tabLinks
             // 
             this.tabLinks.Location = new System.Drawing.Point(4, 25);
             this.tabLinks.Name = "tabLinks";
-            this.tabLinks.Size = new System.Drawing.Size(374, 397);
+            this.tabLinks.Size = new System.Drawing.Size(927, 650);
             this.tabLinks.TabIndex = 3;
             this.tabLinks.Text = "Links";
-            this.tabLinks.UseVisualStyleBackColor = true;
             // 
             // tabSettings
             // 
             this.tabSettings.Location = new System.Drawing.Point(4, 25);
             this.tabSettings.Name = "tabSettings";
-            this.tabSettings.Size = new System.Drawing.Size(374, 397);
+            this.tabSettings.Size = new System.Drawing.Size(927, 618);
             this.tabSettings.TabIndex = 4;
             this.tabSettings.Text = "Settings";
             this.tabSettings.UseVisualStyleBackColor = true;
@@ -106,22 +124,166 @@
             // 
             this.tabAbout.Location = new System.Drawing.Point(4, 25);
             this.tabAbout.Name = "tabAbout";
-            this.tabAbout.Size = new System.Drawing.Size(374, 397);
+            this.tabAbout.Size = new System.Drawing.Size(927, 618);
             this.tabAbout.TabIndex = 5;
             this.tabAbout.Text = "About";
             this.tabAbout.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxList
+            // 
+            this.groupBoxList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxList.Controls.Add(this.panelImageList);
+            this.groupBoxList.Location = new System.Drawing.Point(687, 9);
+            this.groupBoxList.Name = "groupBoxList";
+            this.groupBoxList.Size = new System.Drawing.Size(234, 635);
+            this.groupBoxList.TabIndex = 4;
+            this.groupBoxList.TabStop = false;
+            this.groupBoxList.Text = "Image List";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(681, 650);
+            this.panel1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Commodore C64 (Breadbin)",
+            "Commodore 64C",
+            "Commodore C128",
+            "Commodore C128D"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(208, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hardware";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(10, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 18);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Schematic 1 of 2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 16);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Model";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "250407",
+            "321245"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 96);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(208, 24);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(4, 149);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(225, 91);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Highlight";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 29);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(126, 20);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Highlight in Main";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(11, 55);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(158, 20);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Highlight in Image List";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.Info;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(316, 591);
+            this.label7.Name = "label7";
+            this.label7.Padding = new System.Windows.Forms.Padding(5);
+            this.label7.Size = new System.Drawing.Size(105, 28);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "U27 (3 places)";
+            // 
+            // panelImageList
+            // 
+            this.panelImageList.Location = new System.Drawing.Point(0, 12);
+            this.panelImageList.Name = "panelImageList";
+            this.panelImageList.Size = new System.Drawing.Size(234, 617);
+            this.panelImageList.TabIndex = 7;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.ClientSize = new System.Drawing.Size(1182, 703);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.tabControl1);
-            this.MinimumSize = new System.Drawing.Size(1000, 600);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(1200, 750);
             this.Name = "Main";
-            this.Text = "Main";
+            this.Text = "Commodore Repair Toolbox";
             this.tabControl1.ResumeLayout(false);
+            this.tabMain.ResumeLayout(false);
+            this.groupBoxList.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -134,5 +296,17 @@
         private System.Windows.Forms.TabPage tabLinks;
         private System.Windows.Forms.TabPage tabSettings;
         private System.Windows.Forms.TabPage tabAbout;
+        private System.Windows.Forms.GroupBox groupBoxList;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panelImageList;
     }
 }
--- Main.cs
+++ Main.cs
@@ -12,8 +12,9 @@ namespace Commodore_Retro_Toolbox
         private CustomPanel panelMain;
         private Panel panelImage;
         private PictureBox overlayPictureBox1;
-        private Bitmap overlayBitmap1; 
-        
+        private Bitmap overlayBitmap1;
+//        private PictureBox listBox1;
+
         // Main variables
         private Image image;
         private float zoomFactor = 1.0f;
@@ -32,7 +33,19 @@ namespace Commodore_Retro_Toolbox
         public Main()
         {
             InitializeComponent();
+            InitializeTabMain();
+            InitializeList();
+        }
 
+
+        // ###########################################################################################
+        // InitializeTabMain()
+        // -------------------
+        // Setup the tab named "Main"
+        // ###########################################################################################
+
+        private void InitializeTabMain()
+        {
             // Initialize main panel, make it part of the "tabMain" and fill the entire size
             panelMain = new CustomPanel
             {
@@ -41,7 +54,7 @@ namespace Commodore_Retro_Toolbox
                 AutoScroll = true,
                 Dock = DockStyle.Fill,
             };
-            tabMain.Controls.Add(panelMain);
+            panel1.Controls.Add(panelMain);
 
             // Load an image and initialize image panel
             image = Image.FromFile(Application.StartupPath + "\\Data\\Schematics.gif");
@@ -57,6 +70,7 @@ namespace Commodore_Retro_Toolbox
             // Initialize overlay PictureBox and store its original dimensions
             overlayPictureBox1 = new PictureBox
             {
+                Name = "U3",
                 Size = new Size(250, 410),
                 Location = new Point(1226, 1672),
                 BackColor = Color.Transparent,
@@ -65,8 +79,8 @@ namespace Commodore_Retro_Toolbox
             originalOverlaySize = overlayPictureBox1.Size;
             originalOverlayLocation = overlayPictureBox1.Location;
 
-            
-           
+
+
             /*
             // Create and set a bitmap for the overlay PictureBox
             overlayBitmap1 = new Bitmap(overlayPictureBox1.Width, overlayPictureBox1.Height);
@@ -88,6 +102,8 @@ namespace Commodore_Retro_Toolbox
             overlayPictureBox1.MouseDown += PanelImage_MouseDown;
             overlayPictureBox1.MouseUp += PanelImage_MouseUp;
             overlayPictureBox1.MouseMove += PanelImage_MouseMove;
+            overlayPictureBox1.MouseEnter += new EventHandler(this.Overlay_MouseEnter);
+            overlayPictureBox1.MouseLeave += new EventHandler(this.Overlay_MouseLeave);
 
             // Enable double buffering for smoother updates
             panelMain.DoubleBuffered(true);
@@ -96,8 +112,158 @@ namespace Commodore_Retro_Toolbox
 
 
         // ###########################################################################################
+        // InitializeTabMain()
+        // ------------
+        // Setup the tab named "Main"
+        // ###########################################################################################
+
+        private void InitializeList()
+        {
+
+            panelImageList.AutoScroll = true;
+            panelImageList.Location = new Point(0, 0);
+            panelImageList.Dock = DockStyle.Fill;
+            groupBoxList.Controls.Add(panelImageList);
+
+            // ---
+
+            PictureBox listBox1 = new PictureBox
+            {
+                Size = new Size(167, 110), // Set the size you want
+                Location = new Point(4, 25), // Set the location within the parent control
+                Image = Image.FromFile(Application.StartupPath + "\\Data\\Commodore 64 Breadbin\\250425\\Schematics 1of2.gif"), // Load an image from file
+                SizeMode = PictureBoxSizeMode.Zoom, // Optional: Set how the image should be displayed
+//                BorderStyle = BorderStyle.FixedSingle,
+            };
+
+            // Add the Paint event handler to draw the border
+            listBox1.Paint += new PaintEventHandler((sender, e) =>
+            {
+                float penWidth = 1;
+                using (Pen pen = new Pen(Color.Red, penWidth))
+                {
+                    pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Custom;
+                    pen.DashPattern = new float[] { 4, 2 };
+                    float halfPenWidth = penWidth / 2;
+                    e.Graphics.DrawRectangle(pen, halfPenWidth, halfPenWidth, listBox1.Width - penWidth, listBox1.Height - penWidth);
+                }
+            });
+
+            panelImageList.Controls.Add(listBox1);
+
+            Label listLabel1 = new Label
+            {
+                Text = "Schematics 1 of 2",
+                Location = new Point(0, 0),  // Set the location within the parent control
+                BorderStyle = BorderStyle.FixedSingle,
+                AutoSize = true,
+                BackColor = Color.White,  // Set the background color to Red
+                Padding = new Padding(left:2, top:2, right:2, bottom:2),
+            };
+            listBox1.Controls.Add(listLabel1);
+
+            // ---
+
+            PictureBox listBox2 = new PictureBox
+            {
+                Size = new Size(167, 110), // Set the size you want
+                Location = new Point(4, 140), // Set the location within the parent control
+                Image = Image.FromFile(Application.StartupPath + "\\Data\\Commodore 64 Breadbin\\250425\\Schematics 2of2.gif"), // Load an image from file
+                SizeMode = PictureBoxSizeMode.Zoom, // Optional: Set how the image should be displayed
+                BorderStyle = BorderStyle.FixedSingle,
+            };
+
+            panelImageList.Controls.Add(listBox2);
+
+            Label listLabel2 = new Label
+            {
+                Text = "Schematics 2 of 2",
+                Location = new Point(0, 0),  // Set the location within the parent control
+                BorderStyle = BorderStyle.FixedSingle,
+                AutoSize = true,
+                BackColor = Color.White,  // Set the background color to Red
+                Padding = new Padding(left: 2, top: 2, right: 2, bottom: 2),
+            };
+            listBox2.Controls.Add(listLabel2);
+
+            // ---
+
+            PictureBox listBox3 = new PictureBox
+            {
+                Size = new Size(167, 110), // Set the size you want
+                Location = new Point(4, 255), // Set the location within the parent control
+                Image = Image.FromFile(Application.StartupPath + "\\Data\\Commodore 64 Breadbin\\250425\\Board layout 250425.png"), // Load an image from file
+                SizeMode = PictureBoxSizeMode.Zoom, // Optional: Set how the image should be displayed
+                BorderStyle = BorderStyle.FixedSingle,
+            };
+
+            panelImageList.Controls.Add(listBox3);
+
+            Label listLabel3 = new Label
+            {
+                Text = "Layout",
+                Location = new Point(0, 0),  // Set the location within the parent control
+                BorderStyle = BorderStyle.FixedSingle,
+                AutoSize = true,
+                BackColor = Color.White,  // Set the background color to Red
+                Padding = new Padding(left: 2, top: 2, right: 2, bottom: 2),
+            };
+            listBox3.Controls.Add(listLabel3);
+
+            // ---
+
+            PictureBox listBox4 = new PictureBox
+            {
+                Size = new Size(167, 110), // Set the size you want
+                Location = new Point(4, 370), // Set the location within the parent control
+                Image = Image.FromFile(Application.StartupPath + "\\Data\\Commodore 64 Breadbin\\250425\\Print top.JPG"), // Load an image from file
+                SizeMode = PictureBoxSizeMode.Zoom, // Optional: Set how the image should be displayed
+                BorderStyle = BorderStyle.FixedSingle,
+            };
+
+            panelImageList.Controls.Add(listBox4);
+
+            Label listLabel4 = new Label
+            {
+                Text = "Top",
+                Location = new Point(0, 0),  // Set the location within the parent control
+                BorderStyle = BorderStyle.FixedSingle,
+                AutoSize = true,
+                BackColor = Color.White,  // Set the background color to Red
+                Padding = new Padding(left: 2, top: 2, right: 2, bottom: 2),
+            };
+            listBox4.Controls.Add(listLabel4);
+
+            // ---
+            /*
+            PictureBox listBox5 = new PictureBox
+            {
+                Size = new Size(167, 110), // Set the size you want
+                Location = new Point(4, 490), // Set the location within the parent control
+                Image = Image.FromFile(Application.StartupPath + "\\Data\\Schematics.gif"), // Load an image from file
+                SizeMode = PictureBoxSizeMode.Zoom, // Optional: Set how the image should be displayed
+                BorderStyle = BorderStyle.FixedSingle,
+            };
+
+            panelImageList.Controls.Add(listBox5);
+
+            Label listLabel5 = new Label
+            {
+                Text = "Whatever",
+                Location = new Point(0, 0),  // Set the location within the parent control
+                BorderStyle = BorderStyle.FixedSingle,
+                AutoSize = true,
+                BackColor = Color.White,  // Set the background color to Red
+                Padding = new Padding(left: 2, top: 2, right: 2, bottom: 2),
+            };
+            listBox5.Controls.Add(listLabel5);
+            */
+        }
+
+
+        // ###########################################################################################
         // Main_Shown()
-        // -----------------
+        // ------------
         // What to do AFTER the Main() form has been shown?
         // ###########################################################################################
 
@@ -270,6 +436,23 @@ namespace Commodore_Retro_Toolbox
                 Debug.WriteLine("MouseUp event");
                 lastMousePosition = Point.Empty;
             }
+        }
+
+        private void Overlay_MouseEnter(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Hand;
+            Control control = sender as Control;
+            if (control != null)
+            {
+                label7.Text = control.Name;
+            }
+            label7.Visible = true;
+        }
+
+        private void Overlay_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Default;
+            label7.Visible = false;
         }
 
 
