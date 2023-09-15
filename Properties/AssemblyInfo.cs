ed01e910785c21d5160ee1bcb71bd9225a4f6c64
Dennis <74433222+HovKlan-DH@users.noreply.github.com>
2023-09-16 00:30:14 +0200

    Update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Commodore-Retro-Toolbox")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Commodore Repair Toolbox")]
+[assembly: AssemblyDescription("Repairing Commodore hardware")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Commodore-Retro-Toolbox")]
+[assembly: AssemblyProduct("Commodore Retro Toolbox")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.5")]
+[assembly: AssemblyFileVersion("0.0.0.5")]
